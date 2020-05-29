3121afb28d1b311b8e89e6c85bc84f6af9070ac1
malikH88 <malik.hyamine@gmail.com>
2020-05-30 01:02:17 +0200

    commit5

--- server.js
+++ server.js
@@ -4,3 +4,11 @@ const requestHandler = (request, response) => {
     console.log(request.url);
     response.end('Hello Node.js Server!');
   };
+const server = http.createServer(requestHandler);
+server.listen(port, (err) => {
+    if (err) {
+      console.error('Something bad happened');
+    } else {
+      console.log(`server is listening on ${port}`);
+    }
+  });
